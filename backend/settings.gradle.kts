97422811977c637cab17f0505083d9cefed6ca36
Pedro Arenas <pedroars99@icloud.com>
2022-05-01 19:56:07 +0200

    Restructure backend to a Gradle multiproject

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,15 +1,1 @@
-pluginManagement {
-    val quarkusPluginVersion: String by settings
-    val quarkusPluginId: String by settings
-
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-
-    plugins {
-        id(quarkusPluginId) version quarkusPluginVersion
-    }
-}
-rootProject.name="backend"
+includeBuild("quarkus-app")
