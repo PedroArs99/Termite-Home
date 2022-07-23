66f436cf72e13e2f57c046f1e4e1cb140c2fa76b
Pedro Arenas <48551390+PedroArs99@users.noreply.github.com>
2022-07-23 14:51:04 +0200

    Load Empty Frontend project

---
+++ mqtt-dashboard/ui/src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Sidenav from '$lib/Sidenav.svelte';
+	import '../app.css';
+</script>
+
+<Sidenav />
+
+<main>
+	<slot />
+</main>
+
+<style>
+</style>
---
+++ mqtt-dashboard/ui/src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts"></script>
+
+<svelte:head>
+	<title>MQTT Dashboard</title>
+	<meta name="description" content="Termite Home - MQTT Dashboard" />
+</svelte:head>
+
+<section class="dark:bg-zinc-800">
+</section>
+
+<style>
+</style>
