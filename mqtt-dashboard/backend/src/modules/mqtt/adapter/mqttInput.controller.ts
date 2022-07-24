67e19ad0bd7e7a9d624f55eca3f0b1721140781d
Pedro Arenas <pedroars99@icloud.com>
2022-07-24 02:58:00 +0200

    [MQTT-Dashboard] Fix topic logging

--- mqtt-dashboard/backend/src/modules/mqtt/adapter/mqttInput.controller.ts
+++ mqtt-dashboard/backend/src/modules/mqtt/adapter/mqttInput.controller.ts
@@ -7,8 +7,12 @@ export class MqttInputController {
     constructor(private log: Logger){}
 
     @MessagePattern('$SYS/#')
+    logSysMessage(@Payload() data: any[], @Ctx() context: MqttContext){
+        this.log.debug(`Message received from Topic ${context.getTopic()}: ${data}`)
+    }
+
     @MessagePattern('#')
     logTopicMessage(@Payload() data: any[], @Ctx() context: MqttContext){
-        this.log.debug(`Message received from Topic ${context.getTopic()}: ${data}`)
+        this.log.debug(`Message received from Topic ${context.getTopic()}`)
     }
 }
