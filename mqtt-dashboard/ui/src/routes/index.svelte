9bf7a4987325fa1b9fb358e41dcceb797fc889a8
Pedro Arenas <pedroars99@icloud.com>
2022-07-24 03:40:59 +0200

    [MQTT-Dashboard] Add frontend WebSockets Support

--- mqtt-dashboard/ui/src/routes/index.svelte
+++ mqtt-dashboard/ui/src/routes/index.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+  import { io } from 'socket.io-client';
+  const socket = io('ws://localhost:3000');
+  // client-side
+  socket.on('connect', () => {
+    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+	console.log(socket.connected);
+
+	socket.on('datetime', (payload) => {
+		console.log(payload);
+	})
+  });
+
+  socket.connect();
 </script>
 
-<h1>
-	This is the $Sys topic page
-</h1>
-
+<h1>This is the $Sys topic page</h1>
